96249071c8c6e6da5ad30a07b93c00b766ff7434
FangLinLin338 <62979845+FangLinLin338@users.noreply.github.com>
2022-12-24 19:08:14 +0800

    Add files via upload

---
+++ ImgProc.h
@@ -0,0 +1,44 @@
+#ifndef HUGHIE_ENHANCEMENT_H_
+#define HUGHIE_ENHANCEMENT_H_
+
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <math.h>
+#include <opencv2/opencv.hpp>
+#include <Shlwapi.h>
+#pragma comment(lib, "Shlwapi.lib")
+
+namespace HUGHIE
+{
+	typedef struct BlockInfo
+	{
+		cv::Mat		m_BlkImg;
+		float		m_fMb;
+		float		m_fMr;
+		float		m_fDb;
+		float		m_fDr;
+	}SBlockInfo;
+	class ImgEnhance
+	{
+	public:
+		ImgEnhance() {};
+		~ImgEnhance() {};
+
+		bool Test(const cv::Mat srcImg, cv::Mat &dstImg);											//²âÊÔ½Ó¿Ú
+
+	private:
+		bool CLHE(const cv::Mat CtdL, cv::Mat &HdL, const float fClipRatio = 0.75f);
+		bool ModifyYComponent(const cv::Mat srcImg, const cv::Mat oriL, const cv::Mat dstL, cv::Mat &dstImg);
+	
+		bool SqrtAutoColorLevel(const cv::Mat srcImg, cv::Mat &dstImg);
+
+		bool LogTrans(const cv::Mat srcImg, cv::Mat &dstImg, const float fRatio = 0.01f);
+		bool NormLogL(const cv::Mat oriLogL, cv::Mat &dstL);
+
+	};
+}
+
+#endif // !HUGHIE_ENHANCEMENT_H_
+
